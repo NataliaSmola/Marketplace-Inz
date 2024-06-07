459f870ee39ac31036c0be6032be1a49ef6e18c8
Natalia S <71900790+NataliaSmola@users.noreply.github.com>
2024-06-07 16:52:53 +0200

    Change to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
 import Signup from './components/Signup';
 import Sneakers from './components/Sneakers';
